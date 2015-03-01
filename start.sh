bae4c3c5017ea14ff56a9e882b2dc400a0237e9e
Antti Vesanen <antti.vesanen@iki.fi>
2015-03-01 23:54:39 +0200

    add uploads as a volume

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@
 docker build -t blog .
 docker stop blog
 docker rm blog
-docker run -v /var/lib/blog:/go/src/github.com/avesanen/blog/db -p 5001:5000 -d --name blog blog
+docker run -v /var/lib/blog/uploads:/go/src/github.com/avesanen/blog/uploads \
+           -v /var/lib/blog/db:/go/src/github.com/avesanen/blog/db \
+		   -p 5001:5000 \
+		   -d \
+		   --name blog \
+		   blog
