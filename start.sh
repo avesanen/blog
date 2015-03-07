7a1c3ca681e04bbb148a07ad2deaadf2f839a77b
Antti Vesanen <antti.vesanen@iki.fi>
2015-03-07 17:50:18 +0200

    internal port change

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker stop blog
 docker rm blog
 docker run -v /var/lib/blog/uploads:/go/src/github.com/avesanen/blog/uploads \
            -v /var/lib/blog/db:/go/src/github.com/avesanen/blog/db \
-		   -p 5001:5000 \
+		   -p 5001:80 \
 		   -d \
 		   --name blog \
 		   blog
